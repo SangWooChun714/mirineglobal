cd6f2812e3ae5e4dfd14b28708016203eeaf7b49
SangWoo Cheon <mig55@naver.com>
2022-02-11 17:26:01 +0900

    update mathod

---
+++ pythonfile/kabuconfig.py
@@ -0,0 +1,9 @@
+import logging
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s- %(funcName)s - %(lineno)d - %(name)s - %(levelname)s - %(message)s")
+file_handler = logging.FileHandler("kabulog.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
