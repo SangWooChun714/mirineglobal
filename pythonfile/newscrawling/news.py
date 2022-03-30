d42f69acb5c06fab2f7fced4187d7994c6820d33
csw <mig55@naver.com>
2022-03-30 18:18:42 +0900

    update

--- pythonfile/newscrawling/news.py
+++ pythonfile/newscrawling/news.py
@@ -1,39 +1,52 @@
+from netrc import netrc
 from selenium import webdriver
-from time import sleep
 from datetime import date
 import ssl
 from elasticsearch import Elasticsearch
 from newscofig import logger
 
 
-def newscraling():
-
+def elastic(newspage, n):  
     todays = str(date.today().strftime("%Y.%m.%d")) # 今日の日付を保存
-    
     ctx = ssl.create_default_context()
     ctx.load_verify_locations("C:/Users/cjstk/Desktop/elasticsearch-8.1.0/cert1/http_ca.crt")
     es = Elasticsearch("https://localhost:9200", basic_auth=("elastic","sw1594311") ,ssl_context=ctx)
     index = "dailynews"
 
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    driver1 = webdriver.Chrome("C:/Users/cjstk/Desktop/mirineglobal/chromedriver.exe", options=options)
-
-    driver1.get("https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100")
-    newslink = driver1.find_elements_by_class_name("cluster_text_headline")
-    n = 1
     tempdate = todays.split(".")
     iddate = tempdate[0]+tempdate[1]+tempdate[2]
 
-    for i in newslink:
+    for i in newspage:
         iddate1 = iddate+"("+str(n)+")"
         body = {
             "date" : todays,
             "title" : i.text,
-            "link" : i.get_attribute("href"),
+            "link" : i.get_attribute("href")
         }
         es.index(index = index, id=iddate1, document = body)
         n += 1
+    return n
+
+#ウェブからニュースをcrawlingしてealsticsearchに保存する関数
+#todaysは今日を保存する
+#ctxほelasticsearchの認証書
+#indexはelasticsearchに保存するindex名
+def newscraling():
+
+    n = 1
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver1 = webdriver.Chrome("C:/Users/cjstk/Desktop/mirineglobal/chromedriver.exe", options=options)
+    #driver2 = webdriver.Chrome("C:/Users/cjstk/Desktop/mirineglobal/chromedriver.exe", options=options)
+
+    driver1.get("https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100")
+    politicsnews = driver1.find_elements_by_class_name("cluster_text_headline")
+    n = elastic(politicsnews, n)
+
+    driver1.get(driver1.find_element_by_xpath("/html/body/div[1]/div[3]/div[3]/div/div/div[1]/div/ul/li[3]/a").get_attribute("href"))
+    econews = driver1.find_elements_by_class_name("cluster_text_headline")
+    n = elastic(econews, n)
+
 
 if __name__ == "__main__" :
     logger.info("start news work")
