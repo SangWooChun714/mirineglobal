635ef93ed5a7c10ec509ae31b286c9ac7fcc7a1d
csw <mig55@naver.com>
2022-05-12 17:20:58 +0900

    update

--- pythonfile/newscrawling/searchnetwork.py
+++ pythonfile/newscrawling/searchnetwork.py
@@ -1,72 +1,54 @@
-from selenium.webdriver.chrome.service import Service
-from selenium import webdriver
-import MeCab, datetime
-from time import sleep
+import MeCab, datetime, requests, logging
 from wordcloud import WordCloud
-#import newscofig
+from bs4 import BeautifulSoup
+
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s- %(funcName)s - %(lineno)d - %(name)s - %(levelname)s - %(message)s")
+file_handler = logging.FileHandler("websearchnews.log", encoding="utf-8")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 date = str(datetime.date.today())
-#logger = newscofig.logger
-#ネットに接続するdriverを作ってreturnするメソッド
-#optionsでchromeのwindowを開かずにする
-def setdriver(search):
-    #logger.info("set driver")
-    options = webdriver.ChromeOptions()
-    options.add_argument("headless")
-    chrome_service = Service(executable_path="C:/Users/mg-e1/Desktop/mg/chromedriver.exe")
-    driver = webdriver.Chrome(service=chrome_service, options = options)
-    driver.get("https://news.yahoo.co.jp/search?p="+search+"&ei=utf-8") #住所で接続してページの情報を表示する
-    
-    #logger.info("return driver")
-    return driver
 
-#形態素解析をするメソッド
-#driverで接続したページの中のニュース記事の住所を持ってきて記事の内容を形態素解析する
-def mecabs(news, driver):
-    #logger.info("start mecab")
-    formecab = ""
-    links = [n.get_attribute("href") for n in news]
+def searchnetwork(search):
 
-    for link in links :
-        #logger.info("now link : "+link)
-        driver.execute_script('window.open("'+link+'");')
-        driver.switch_to.window(driver.window_handles[1])
+    logger.info("start searchnetwork")
+    if search =="search is empty":
+        url = "https://news.yahoo.co.jp/"
+    else : 
+        url = "https://news.yahoo.co.jp/search?p="+search+"&ei=utf-8" 
+    words = ""
+    __headers = {"user-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"}#heaaderを指定
+    res1 = requests.get(url=url, headers=__headers)
+    searchday = BeautifulSoup(res1.text, "lxml")
+    divnews = searchday.find_all("div", attrs={"class":"newsFeed_item_text"})# tableから資料を持ってくる
+    for i , j in enumerate(divnews):
+        newstitle = j.find("div", attrs={"class":"newsFeed_item_title"}) # tableから資料を持ってくる
+        newsbody = j.find("div", attrs={"class":"sc-cnyaSH kPLsVs"})
+        words = words + newstitle.text + newsbody.text
+    words = words.replace(" ", "")
+    words = words.replace("\n", "")
+    logger.info("end searchnetwork")
+    return words
 
-        try :  
-            script = driver.find_element_by_xpath("/html/body/div[1]/div/main/div[1]/div/article/div[1]").text
-        except Exception as e:
-            #logger.warn(e)
-            continue
-
-        script = script.replace(" ", "")
-        script = script.replace("\n", "")
-
-        tempmecab = MeCab.Tagger().parse(script).splitlines()
-
-        for line in tempmecab :
-            if "名詞" in line : #名詞だけをformecabに保存する
-                temp = line.split()[0]
-                temp = temp.replace("'","")
-                formecab = formecab +" "+ temp
-        sleep(10)
-    #logger.info("end mecab")
-    return formecab
-
-#wordcloudを作成するメソッド
-#driverでネットに接続、mecabsで名詞だけを持って来てwordcloudにする
 def wordcloudmethod(search):
-    print(search)
-    #logger.info("start wordcloud")
-    driver = setdriver(search)
-    news = driver.find_elements_by_class_name("viewableWrap newsFeed_item newsFeed_item-normal newsFeed_item-ranking ")
-    words = mecabs(news, driver)
-    #logger.info("draw wordcloud")
+
+    logger.info("start wordcloud")
+    words = searchnetwork(search)
+    tempmecab = MeCab.Tagger().parse(words).splitlines()
+    mecabs = ""
+    for line in tempmecab :
+        if "名詞" in line : #名詞だけをformecabに保存する
+            temp = line.split()[0]
+            temp = temp.replace("'","")
+            mecabs = mecabs +" "+ temp
+    
     wc = WordCloud(width=400, height=400, scale=2.0, max_font_size=250, font_path="C:/Windows/Fonts/HGRGE.TTC")
-    wc.generate(words)
-    wc.to_file('./static/image/word('+date+').jpg')
-    #logger.info("end wordcloud")
-
-
-
-
-
+    ad =wc.generate(mecabs)
+    wc.to_file('./static/image/'+search+'word('+date+').jpg')
+    filenames = search+'word('+date+').jpg'
+    logger.info("end wordcloud")
+    return filenames
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from dataclasses import replace
 from flask import Flask, render_template, request
+from pymysql import NULL
 from pythonfile.newscrawling import searchnetwork
+
+
+
 app = Flask(__name__)
 
 @app.route("/")#웹 브라우저에서 웹서버로 접근 시 / 가 입력되면 이 함수가 실행되도록 함.
@@ -9,12 +12,13 @@ def hello_World():
 
 @app.route("/search", methods=["POST"]) # /search 가 입력되면 이 함수를 실행하고 내용을 post방식으로 받아온다
 def searchkabuweb():
-    search = request.form['kabuname'] # 서버의 request에서 kabuname이라는 변수를 받아온다 이것은 html의 name태그를 지정하여 받아온다.
-    date = request.form["date"]
-    date = date.replace("-",".")
-    print(search)
-    print(date)
-    #searchnetwork.wordcloudmethod(search)
+    if request.form['kabuname'] == "":
+        search = "search is empty"
+        print("succes")
+    else : 
+        search = request.form['kabuname'] # 서버의 request에서 kabuname이라는 변수를 받아온다 이것은 html의 name태그를 지정하여 받아온다.
+    print(search+" 1")
+    files = searchnetwork.wordcloudmethod(search)
     #result = searchkabu.searchkabu(kabuname) #pythonfile 폴더의 searchkabu.py를 import하여 파일 내부의 searchkabu라는 함수에 kabuname을 넣어 결과를 result에 저장
     # code = result[0] # 상장된 회사의 코드
     # nowval = result[1] # 현재 주식 가격
@@ -22,7 +26,7 @@ def searchkabuweb():
     # upper = result[3] # 전날에 비해 상승인지 하락인지를 나타냄
     # differ = result[4] # 전날에 비교한 가격변동
     #return render_template("search.html", kabuname = kabuname, Code = code, Nowval = nowval, Qunt = qunt, Upper = upper, Differ = differ) 
-    return render_template("search.html", filenames = "image/word(2022-05-11).jpg" )
+    return render_template("search.html", filenames = 'image/'+files, search = search )
     # templates 폴더 내부의 search.html 로 이동하며 request에 변수를 넣어줌
 
 app.run(host="localhost", port=2500) # 서버 시작코드
