55855050b86438332e3014f16f22142217f14d8d
csw <mig55@naver.com>
2022-05-18 17:46:10 +0900

    update

--- mgproject/mongocrawling.py
+++ mgproject/mongocrawling.py
@@ -1,3 +1,4 @@
+from distutils.log import warn
 from io import StringIO
 from mongoconfig import logger
 import requests, datetime
@@ -17,19 +18,21 @@ from pymongo import MongoClient
 #午前に作動したらtableが一つだけなので強制的に2にして1回でcrawlingが終了するようにします。
 def gettablenum(today):
     logger.info("start gettablenum")
+    try : 
+        url = "https://www.release.tdnet.info/inbs/I_list_001_"+today+".html"
+        headers = {"user-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"}#heaaderを指定
+        requst = requests.get(url=url, headers=headers)
+        searchurl = BeautifulSoup(requst.text, "lxml")
+        tablenum = searchurl.find_all("div", attrs={"class" : "pager-M"})
 
-    url = "https://www.release.tdnet.info/inbs/I_list_001_"+today+".html"
-    headers = {"user-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"}#heaaderを指定
-    requst = requests.get(url=url, headers=headers)
-    searchurl = BeautifulSoup(requst.text, "lxml")
-    tablenum = searchurl.find_all("div", attrs={"class" : "pager-M"})
+        if not tablenum : 
+            tablenum = 2
+        else : 
+            tablenum = int(tablenum[-1].get_text())+1
+    except Exception as e:
+        logger.warn(e)
 
-    if not tablenum : 
-        tablenum = 2
-    else : 
-        tablenum = int(tablenum[-1].get_text())
-
-    logger.info("end gettablenum")
+    logger.info("end gettablenum : "+str(tablenum))
     return tablenum
 
 #TDnetから情報を持ってくる関数。
@@ -42,28 +45,29 @@ def geturls():
     today = str(datetime.date.today().strftime("%Y%m%d"))
     result = []
     tablenum = gettablenum(today)
+    try : 
+        for i in range(1, tablenum):
 
-    for i in range(1, tablenum):
+            url = "https://www.release.tdnet.info/inbs/I_list_00"+str(i)+"_"+today+".html"
+            headers = {"user-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"}#heaaderを指定
+            requst = requests.get(url=url, headers=headers)
+            requst.encoding = "utf-8"
+            searchurl = BeautifulSoup(requst.text, "lxml")
+            tempurl1 = searchurl.find("table", attrs={"id" : "main-list-table"})
+            tempurl = tempurl1.find_all("tr")
 
-        url = "https://www.release.tdnet.info/inbs/I_list_00"+str(i)+"_"+today+".html"
-        headers = {"user-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"}#heaaderを指定
-        requst = requests.get(url=url, headers=headers)
-        requst.encoding = "utf-8"
-        searchurl = BeautifulSoup(requst.text, "lxml")
-        tempurl1 = searchurl.find("table", attrs={"id" : "main-list-table"})
-        tempurl = tempurl1.find_all("tr")
-
-        for j in tempurl :
-            #tdのclass名がoddnew, evennewで分けて入れられているのでif文で選別する。
-            if not j.find(attrs = "oddnew-M kjCode") :
-                result.append({"codes" : j.find(attrs = "evennew-M kjCode").get_text(), "names" : j.find(attrs = "evennew-M kjName").get_text().replace(" ", ""), 
-                "date" : today, "titles" : j.find(attrs = "evennew-M kjTitle").get_text().replace(" ", ""), "urls" : "https://www.release.tdnet.info/inbs/"+j.find("a").get("href"), 
-                "script" : " "})
-            else : 
-                result.append({"codes" : j.find(attrs = "oddnew-M kjCode").get_text(), "names" : j.find(attrs = "oddnew-M kjName").get_text().replace(" ", ""), 
-                "date" : today, "titles" : j.find(attrs = "oddnew-M kjTitle").get_text().replace(" ", ""), "urls" : "https://www.release.tdnet.info/inbs/"+j.find("a").get("href"), 
-                "script" : " "})
-
+            for j in tempurl :
+                #tdのclass名がoddnew, evennewで分けて入れられているのでif文で選別する。
+                if not j.find(attrs = "oddnew-M kjCode") :
+                    result.append({"codes" : j.find(attrs = "evennew-M kjCode").get_text(), "names" : j.find(attrs = "evennew-M kjName").get_text().replace(" ", ""), 
+                    "date" : today, "titles" : j.find(attrs = "evennew-M kjTitle").get_text().replace(" ", ""), "urls" : "https://www.release.tdnet.info/inbs/"+j.find("a").get("href"), 
+                    "script" : " "})    
+                else : 
+                    result.append({"codes" : j.find(attrs = "oddnew-M kjCode").get_text(), "names" : j.find(attrs = "oddnew-M kjName").get_text().replace(" ", ""), 
+                    "date" : today, "titles" : j.find(attrs = "oddnew-M kjTitle").get_text().replace(" ", ""), "urls" : "https://www.release.tdnet.info/inbs/"+j.find("a").get("href"),     
+                    "script" : " "})
+    except Exception as e : 
+        logger.warn(e)
     logger.info("end geturl")
 
     return result
@@ -71,56 +75,66 @@ def geturls():
 #pdfファイルを読んでStringに変換して返す関数。
 def readpdfs(result):
     logger.info("start readpdf")
-    for link in result:
 
-        tempfiles = urlopen(link['urls']).read()
-        files = BytesIO(tempfiles)
-        retstr = StringIO()
-        parser = PDFParser(files)
-        doc = PDFDocument(parser)
-        rsrcmgr = PDFResourceManager()
-        device = TextConverter(rsrcmgr, retstr, laparams=LAParams())
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        for page in PDFPage.create_pages(doc):
-            interpreter.process_page(page)
+    try:
+        for link in result:
 
-        temp = retstr.getvalue()
+            tempfiles = urlopen(link['urls']).read()
+            files = BytesIO(tempfiles)
+            retstr = StringIO()
+            parser = PDFParser(files)
+            doc = PDFDocument(parser)
+            rsrcmgr = PDFResourceManager()
+            device = TextConverter(rsrcmgr, retstr, laparams=LAParams())
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            for page in PDFPage.create_pages(doc):
+                interpreter.process_page(page)
 
-        temp = temp.replace("  ", "")
-        temp = temp.replace("\n \n", "\n")
+            temp = retstr.getvalue()
 
-        link["script"] = temp
+            temp = temp.replace("  ", "")
+            temp = temp.replace("\n \n", "\n")
 
-        retstr.close()
+            link["script"] = temp
+
+            retstr.close()
+    except Exception as e : 
+        logger.warn(e)
+
     logger.info("end readpdf")
     return result
 
 #DBにDataを保存する関数。
 def savedb(result):
     logger.info("start savedb")
-    today = str(datetime.date.today().strftime("%Y%m%d"))
-    clients = MongoClient("mongodb://root:example@localhost:27017/") #db connect
-    db = clients.mongo_crontab #db指定
-    n = 1
-    for result in result : 
-        datas = {
-            "_id" : today+"-"+str(n),
+    
+    try : 
+        today = str(datetime.date.today().strftime("%Y%m%d"))
+        clients = MongoClient("mongodb://root:example@localhost:27017/") #db connect
+        db = clients.mongo_crontab #db指定
 
-            "compy" : { 
-                "code" : result["codes"], 
-                "name" : result["names"] 
-            },
+        n = 1
+        for result in result : 
+            datas = {
+                "_id" : today+"-"+str(n),
 
-            "date" : result["date"],
+                "compy" : { 
+                    "code" : result["codes"], 
+                    "name" : result["names"] 
+                },
 
-            "document" : { 
-                "link" : result["urls"], 
-                "title" : result["titles"],
-                "script" : result["script"]
+                "date" : result["date"],
+
+                "document" : { 
+                    "link" : result["urls"], 
+                    "title" : result["titles"],
+                    "script" : result["script"]
+                }   
             }
-        }
-        db.tdnet.insert_one(datas)
-        n += 1
+            db.tdnet.insert_one(datas)
+            n += 1
+    except Exception as e : 
+        logger.warn(e)
 
 
     logger.info("end savedb")
--- mgproject/mongotest.py
+++ mgproject/mongotest.py
@@ -1,3 +1,7 @@
+from genericpath import exists
+from queue import Empty
+from tabnanny import check
+from numpy import empty
 from pymongo import DESCENDING, MongoClient
 from mongoconfig import logger
 import datetime
@@ -5,15 +9,20 @@ import datetime
 #clients = MongoClient("mongodb+srv://csw1594311:csw1594311@cluster0.wp4cz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 clients = MongoClient("mongodb://root:example@localhost:27017/")
 print(clients.list_database_names())
-today = str(datetime.date.today().strftime("%Y%m%d"))
+today = datetime.date.today().strftime("%Y%m%d")
 db = clients.mongo_crontab
 datas = {
+    "_id":today+"-"+"500",
     "compy" : { "code" : "1123", "name" : "ab1c" },
     "date" : "20220518",
     "document" : { "link" : "htt1p:", 
     "title" : "ab1c",
     "script" : "ahamo1"}
 }
+abc = db.tdnet.find({"compy.code":"1123"})
+if  abc :
+    print(abc)
+    db.tdnet.delete_many({"compy.code":"1123"})
 #db.tdnet.insert_one(datas)
 #fromdata = db.tdnet.find({},{"compy":False, "date":False, "document":False})
 #fromdata = db.tdnet.find({},{"_id":False,"compy":False, "date":False, "document":False}).sort("id",-1)
